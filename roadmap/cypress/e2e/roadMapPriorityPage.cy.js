857a811556861259b72a1cfc5a168f8516d806ba
Andrew Russell <Andrew.Russell@chillisoft.co.za>
2022-11-11 15:03:17 +0200

    refactor tests

--- roadmap/cypress/e2e/roadMapPriorityPage.cy.js
+++ roadmap/cypress/e2e/roadMapPriorityPage.cy.js
@@ -2,148 +2,152 @@ import redis from "../../lib/redis";
 
 describe("Road map priorities", () => {
   const HOME_PAGE = "http://localhost:3000/";
-  beforeEach(async () => {
-    await cy.request("http://localhost:3000/api/flush-all");
+  beforeEach(() => {
+    cy.request("http://localhost:3000/api/flush-all");
   });
 
   describe("given a user inputs a new task and clicks the request button", () => {
     it("should clear the request input", () => {
-      cy.visit(HOME_PAGE);
-      cy.get("#request_input").type("Hello World.");
-      cy.get("#request_button").click();
+      visitHomePage().then(() => {
+        getRequestInput().type("Hello World.");
+        getRequestButton().click();
 
-      cy.get("#request_input").should("contain", "");
+        getRequestInput().should("contain", "");
+      })
     });
 
     it("should add request to list when submit is clicked", () => {
-      cy.visit(HOME_PAGE);
-      cy.get("#request_input").type("Hello World.");
-      cy.get("#request_button").click();
+      visitHomePage().then(() => {
+        getRequestInput().type("Hello World.");
+        getRequestButton().click();
 
-      cy.get(".w-full").find(".record-item").should("have.length", 1);
+        cy.get(".w-full").find(".record-item").should("have.length", 1);
+      });
     });
 
     it("should automatically have your vote.", () => {
-      cy.visit(HOME_PAGE);
-      cy.get("#request_input").type(
-        "request should automatically have your vote."
-      );
-      cy.get("#request_button").click();
+      visitHomePage().then(() => {
+        getRequestInput().type(
+          "request should automatically have your vote."
+        );
+        getRequestButton().click();
 
-      cy.get(".w-full")
-        .find(".record-item .bg-green-100")
-        .should("have.length", 1);
+        cy.get(".w-full")
+          .find(".record-item .bg-green-100")
+          .should("have.length", 1);
+      });
     });
 
     it("should set the vote count to 1.", () => {
-      cy.visit(HOME_PAGE);
-      cy.get("#request_input").type(
-        "request should automatically have your vote."
-      );
-      cy.get("#request_button").click();
+      visitHomePage().then(() => {
+        getRequestInput().type("request should automatically have your vote.");
+        getRequestButton().click();
 
-      cy.get("#feature_score").then((el) => {
-        expect(el).to.have.text(1);
+        getFeatureScore().then((el) => {
+          expect(el).to.have.text(1);
+        });
       });
     });
 
     it("should show validation message when request input is empty.", () => {
-      cy.visit(HOME_PAGE);
-      cy.get("#request_button").click();
+      visitHomePage().then(() => {
+        getRequestButton().click();
 
-      cy.get("#request_input").then((elm) => {
-        expect(elm[0].validationMessage).to.eq("Please fill out this field.");
+        getRequestInput().then((elm) => {
+          expect(elm[0].validationMessage).to.eq("Please fill out this field.");
+        });
       });
     });
 
     it("error toast should be displayed when request exceeds 150 characters.", () => {
-      cy.visit(HOME_PAGE);
-      cy.get("#request_input").type(getTextWithLengthGreaterThan150());
-      cy.get("#request_button").click();
+      visitHomePage().then(() => {
+        getRequestInput().type(getTextWithLengthGreaterThan150());
+        getRequestButton().click();
 
-      cy.get(".go1415219401").then((elm) => {
-        expect(elm[0].innerHTML).to.eq("Max 150 characters please.");
+        cy.get(".go1415219401").then((elm) => {
+          expect(elm[0].innerHTML).to.eq("Max 150 characters please.");
+        });
       });
     });
 
     it("email textbox should be cleared on email submit.", () => {
-      cy.visit(HOME_PAGE);
-      cy.get("#email").type("mail@mail.com");
-      cy.get("#email_submit").click();
+      visitHomePage().then(() => {
+        getEmail().type("mail@mail.com");
+        getEmailSubmit().click();
 
-      cy.get("#email").should("contain", "");
+        getEmail().should("contain", "");
+      });
     });
     it("should display success message when email is submitted.", () => {
-      cy.visit(HOME_PAGE);
-      cy.get("#email").type("mail@mail.com");
-      cy.get("#email_submit").click();
+      visitHomePage().then(() => {
+        getEmail().type("mail@mail.com");
+        getEmailSubmit().click();
 
-      cy.get(".go1415219401").then((elm) => {
-        expect(elm[0].innerHTML).to.eq(
-          "You are now subscribed to feature updates!"
-        );
+        getEmailSuccessMessage().then((elm) => {
+          expect(elm[0].innerHTML).to.eq(
+            "You are now subscribed to feature updates!"
+          );
+        });
+
+        getEmailSuccessMessage()
+          .invoke("attr", "aria-live")
+          .should("eq", "polite");
       });
-      cy.get(".go1415219401")
-        .invoke("attr", "aria-live")
-        .should("eq", "polite");
     });
 
     it("should show validation message when email input is empty.", () => {
-      cy.visit(HOME_PAGE);
-      cy.get("#email_submit").click();
+      visitHomePage().then(() => {
+        getEmailSubmit().click();
 
-      cy.get("#email").then((elm) => {
-        expect(elm[0].validationMessage).to.eq("Please fill out this field.");
+        getEmail().then((elm) => {
+          expect(elm[0].validationMessage).to.eq("Please fill out this field.");
+        });
       });
     });
 
     it("should show validation message when email does not have @ sign.", () => {
-      cy.visit(HOME_PAGE);
-      cy.get("#email").type("hello");
-      cy.get("#email_submit").click();
+      visitHomePage().then(() => {
+        getEmail().type("hello");
+        getEmailSubmit().click();
 
-      cy.get("#email").then((elm) => {
-        expect(elm[0].validationMessage).to.eq(
-          "Please include an '@' in the email address. 'hello' is missing an '@'."
-        );
+        getEmail().then((elm) => {
+          expect(elm[0].validationMessage).to.eq(
+            "Please include an '@' in the email address. 'hello' is missing an '@'."
+          );
+        });
       });
     });
 
     it("should show validation message when email does not have domain.", () => {
-      cy.visit(HOME_PAGE);
-      cy.get("#email").type("hello@");
-      cy.get("#email_submit").click();
+      visitHomePage().then(() => {
+        getEmail().type("hello@");
+        getEmailSubmit().click();
 
-      cy.get("#email").then((elm) => {
-        expect(elm[0].validationMessage).to.eq(
-          "Please enter a part following '@'. 'hello@' is incomplete."
-        );
+        getEmail().then((elm) => {
+          expect(elm[0].validationMessage).to.eq(
+            "Please enter a part following '@'. 'hello@' is incomplete."
+          );
+        });
       });
     });
 
-    it("vote count should increase by one if vote already exists", async () => {
-      await cy.request({
-        method: "POST",
-        url: "http://localhost:3000/api/create",
-        headers: {
-          "x-forwarded-for": "192.168.0.50",
-        },
-        body: {
-          title: "a cool request 3",
-        },
-      });
-      cy.visit(HOME_PAGE);
-      cy.get("#vote_button").click();
-      cy.get("#feature_score").should("contain", 2);
-      cy.get("#vote_button").then((elm) => {
-        expect(elm[0].className).to.include(
-          "bg-green-100"
-        );
-      });
+    it("vote count should increase by one if vote already exists", () => {
+      addRequest('a cool request 3')
+        .then(() => {
+          visitHomePage().then(() => {
+            getVoteButton().click();
+            getFeatureScore().should("contain", 2);
+            getVoteButton().then((elm) => {
+              expect(elm[0].className).to.include(
+                "bg-green-100"
+              );
+            });
+          });
+        })
     });
 
 
-    it.only("features should have been re-ordered", () => {
+    it("features should have been re-ordered", () => {
       addRequest('Ireland to win by 5')
         .then(() => addRequest('Springboks to draw with Ireland'))
         .then(() => addRequest('Springboks to win by 5'))
@@ -176,6 +180,34 @@ describe("Road map priorities", () => {
         });
 
     });
+
+    const getVoteButton = () => {
+      return cy.get("#vote_button");
+    }
+
+    const getEmail = () => {
+      return cy.get("#email");
+    }
+
+    const getEmailSubmit = () => {
+      return cy.get("#email_submit");
+    }
+
+    const getEmailSuccessMessage = () => {
+      return cy.get(".go1415219401");
+    }
+
+    const getFeatureScore = () => {
+      return cy.get("#feature_score");
+    }
+
+    const getRequestButton = () => {
+      return cy.get("#request_button");
+    }
+
+    const getRequestInput = () => {
+      return cy.get("#request_input");
+    }
 
     const getFeatureByTitleFrom = (features, title) => {
       return features.find((f) => f.title === title)
